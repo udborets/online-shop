9d55640acef5e3439dd151c1d9278b44bbb0b909
Yury Borets <udborets@gmail.com>
2023-02-07 11:51:42 +0300

    add controller classes for pg table models

---
+++ server/controllers/brandController.js
@@ -0,0 +1,10 @@
+class BrandController {
+  async create(req, res) {
+    
+  }
+  async getAll(req, res) {
+
+  }
+}
+
+export default new BrandController ;
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,11 +1,9 @@
 import { Router } from "express";
-
+import userConroller from "../controllers/userConroller.js";
 const router = Router();
 
-router.post('/registration', )
-router.post('/login', )
-router.get('/auth', (req, res) => {
-  res.json({message: "helloing from userRouter"})
-})
+router.post('/registration', userConroller.registration)
+router.post('/login', userConroller.login)
+router.get('/auth', userConroller.check)
 
 export default router
