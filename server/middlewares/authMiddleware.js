3ecdf16f5f69d173cc9c0d4554c050094c34ee8b
Yury Borets <udborets@gmail.com>
2023-02-07 21:34:33 +0300

    add auth middleware for checking if user is authorized

---
+++ server/middlewares/authMiddleware.js
@@ -0,0 +1,23 @@
+import { verify } from 'jsonwebtoken';
+import { config } from 'dotenv';
+
+config()
+
+export default function (req, res, next) {
+  if (req.method === "OPTIONS") {
+    next();
+  }
+  try {
+    // headers rules are like " Bearer *token* so we split it by space"
+    const token = req.headers.authorisation.split(' ')[1];
+    if (!token) {
+      return res.status(401).json({ message: "Пользователь не авторизован" })
+    }
+    const decoded = jwt.verify(token, process.env.JWT_KEY)
+    req.user = decoded;
+    next()
+  }
+  catch (err) {
+    res.status(401).json({ message: "Пользователь не авторизован" })
+  }
+}
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import userConroller from "../controllers/userController.js";
+import authMiddleware from "../middlewares/authMiddleware.js";
+
 const router = Router();
 
 // handling request functions
 router.post('/registration', userConroller.registration);
 router.post('/login', userConroller.login);
-router.get('/auth', userConroller.check);
+router.get('/auth', authMiddleware, userConroller.check);
 
 export default router
