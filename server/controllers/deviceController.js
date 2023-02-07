523a3ca3acab6be6a7f6ef42421a235754aea2be
Yury Borets <udborets@gmail.com>
2023-02-07 17:53:35 +0300

    add function for     console.log(req.body.name)

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -3,41 +3,40 @@ import path from "path";
 import { v4 } from 'uuid';
 import models from '../models/models.js';
 
+
 class DeviceController {
-  // creates device in db by request
   async create(req, res, next) {
     try {
       const { name, price, brandId, typeId, info } = req.body;
       let { img } = req.files;
       const fileName = v4() + '.jpg';
-      img.mv(path.resolve(path.resolve(), "server", "..", "static", fileName))
-
-      const newDevice = await models.Device.create({ name, price, brandId, typeId, img: fileName })
-      return res.json(newDevice)
+      img.mv(path.resolve(path.resolve(), "server", "..", "static", fileName));
+      console.log(req.body);
+      const newDevice = await models.Device.create({ name, price, brandId, typeId, img: fileName });
+      console.log(123123123);
+      return res.json(newDevice);
     }
     catch (err) {
-      next(ApiError.badRequest(err.message))
+      next(ApiError.badRequest(err.message));
     }
   }
   async getAll(req, res) {
     const { brandId, typeId } = req.query;
     let devices;
-    switch (!!brandId, !!typeId) {
-      case (false, false):
-        devices = await models.Device.findAll();
-        return;
-      case (false, true):
-        devices = await models.Device.findAll({where: {typeId}});
-        return;
-      case (true, false):
-        devices = await models.Device.findAll({where: {brandId}});
-        return;
-      case (true, true):
-        devices = await models.Device.findAll();
-        return;
+    if (!brandId && !typeId) {
+      devices = await models.Device.findAll();
     }
+    if (!brandId && typeId) {
+      devices = await models.Device.findAll({where: typeId});
+    }
+    if (brandId && !typeId) {
+      devices = await models.Device.findAll({where: brandId});
+    }
+    if (brandId && typeId) {
+      devices = await models.Device.findAll({where: {typeId, brandId}});
+    }
+    return res.json(devices);
   }
-  // returning one device by id in request
   async getOne(req, res) {
 
   }
