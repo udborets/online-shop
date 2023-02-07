6ab1486cf6a91744230cef0c671bf89ed9f71c2b
Yury Borets <udborets@gmail.com>
2023-02-07 15:04:27 +0300

    add device control query function for POST method

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -1,6 +1,26 @@
+import ApiError from '../errors/ApiError.js';
+import path from "path";
+import {v4} from 'uuid';
+import models from '../models/models.js';
+
 class DeviceController {
-  async create(req, res) {
-    
+  async create(req, res, next) {
+    try {
+      const { name, price, brandId, typeId, info } = req.body;
+      let { img } = req.files;
+      // generating img filename
+      const fileName = v4() + '.jpg';
+      // adding img to static folder
+      img.mv(path.resolve(path.resolve(), "server", "..", "static", fileName))
+  
+      // creating device object 
+      // not setting rating because it's set to 0 by default
+      const device = await models.Device.create({name, price, brandId, typeId, img: fileName})
+      return res.json(device)
+    } 
+    catch (err) {
+      next(ApiError.badRequest(err.message))
+    }
   }
   async getAll(req, res) {
 
