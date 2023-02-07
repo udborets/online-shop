c4981e3bbcf356c39355521f8bab41b79f5d8b33
Yury Borets <udborets@gmail.com>
2023-02-07 12:48:42 +0300

    add img filename generation

--- server/controllers/userConroller.js
+++ server/controllers/userConroller.js
@@ -1,9 +1,11 @@
 import ApiError from '../errors/ApiError.js';
-
+import UUID from 'uuid'
 
 class UserController {
   async registration(req, res) {
-
+    const { name, price, brandId, typeId, info } = req.body;
+    const { img } = req.files;
+    const fileName = UUID.v4() + '.jpg'
   }
   async login(req, res) {
 
