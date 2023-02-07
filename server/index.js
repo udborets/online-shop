32120c6efb829eb116663bd8013ff8ca850e9ce0
Yury Borets <udborets@gmail.com>
2023-02-07 12:43:25 +0300

    add express-fileupload library

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import {config} from 'dotenv';
 import sequelize from './db.js';
 import models from './models/models.js';
+import fileUpload from 'express-fileupload';
 import cors from 'cors';
 import router from './routes/index.js';
 import errorHandler from './middlewares/ErrorHandlingMiddleware.js';
