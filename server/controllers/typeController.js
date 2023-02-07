7636b6ac2c55fd679716e193495591b77d1a9ea3
Yury Borets <udborets@gmail.com>
2023-02-07 12:30:10 +0300

    add create and getAll query functions

--- server/controllers/typeController.js
+++ server/controllers/typeController.js
@@ -1,9 +1,15 @@
+import models from '../models/models.js';
+import ApiError from '../errors/ApiError.js';
+
 class TypeController {
   async create(req, res) {
-    
+    const { name } = req.body;
+    const type = await models.Type.create({name});
+    return res.json(type)
   }
   async getAll(req, res) {
-
+    const types = await models.Type.findAll();
+    return res.json(types)
   }
 }
 
