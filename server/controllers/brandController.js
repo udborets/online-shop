88c96d8926a2d20c6a2fbc6752ae3d6070a07435
Yury Borets <udborets@gmail.com>
2023-02-07 12:40:42 +0300

    add query functions

--- server/controllers/brandController.js
+++ server/controllers/brandController.js
@@ -1,10 +1,16 @@
+import models from '../models/models.js';
+import ApiError from '../errors/ApiError.js';
+
 class BrandController {
   async create(req, res) {
-    
+    const { name } = req.body;
+    const brand = await models.Brand.create({ name });
+    return res.json(brand);
   }
   async getAll(req, res) {
-
+    const brands = await models.Brand.findAll();
+    return res.json(brands);
   }
 }
 
-export default new BrandController ;
+export default new BrandController;
