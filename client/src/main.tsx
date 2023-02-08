98f7ea4284e38c946c20be9f08be4816cd7c3d72
Yury Borets <udborets@gmail.com>
2023-02-08 22:16:36 +0300

    delete index.scss

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles/index.scss';
 import { Provider } from 'react-redux';
 import store from "./store/index";
 import App from './App';
