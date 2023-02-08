d30a47c92f6d5922ecabdd1cb0ac27b62fa222e8
Yury Borets <udborets@gmail.com>
2023-02-08 12:28:47 +0300

    add user from redux store

--- client/src/components/AppRouter.tsx
+++ client/src/components/AppRouter.tsx
@@ -1,15 +1,24 @@
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { IRoute } from '../models/IRoute';
+import IUserState from '../models/IUserState';
 import { authRoutes, publicRoutes } from '../PageRoutes';
+import { setIsAuth } from '../store/userSlice';
 import { RouteConsts } from '../utils/consts';
 
 const AppRouter = () => {
-  const isAuth = false;
+  const user = useSelector((state: IUserState) => state.user);
+  const dispatch = useDispatch();
+  function toggleIsAuth() {
+    dispatch(setIsAuth({isAuth: !user.isAuth}));
+  }
+  console.log(user)
+
   return (
     <Routes>
       <Route path="*" element={<Navigate to={RouteConsts.SHOP}/>}/>
-      {isAuth && authRoutes.map(({ path, Component }: IRoute) => (
+      {user.isAuth && authRoutes.map(({ path, Component }: IRoute) => (
         <Route key={path} element={<Component />} path={path} />
       ))}
       {publicRoutes.map(({ path, Component }: IRoute) => (
