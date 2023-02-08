a9a48162f56653b4be4da59cf50222ed5b4f3c16
Yury Borets <udborets@gmail.com>
2023-02-08 14:42:43 +0300

    add check if user is logged in

--- client/src/components/NavBar/NavBar.tsx
+++ client/src/components/NavBar/NavBar.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { RouteConsts } from '../../utils/consts';
 import './NavBar.scss';
+import { useSelector } from 'react-redux/es/exports';
+import IUserState from './../../models/IUserState';
 
 const NavBar = () => {
+  const user = useSelector((state: IUserState) => state.user);
   return (
     <div className='nav-bar'>
       <NavLink
@@ -13,11 +16,28 @@ const NavBar = () => {
         Internet shop
       </NavLink>
       <ul className='nav-bar__list'>
-        <li className='nav-bar__link'>Auth</li>
-        <li className='nav-bar__link'>Basket</li>
-        <li className='nav-bar__link'>Device</li>
-        <li className='nav-bar__link'>Shop</li>
+        {
+          user.isAuth
+            ?
+            <>
+              <li className='nav-bar__link'>Basket</li>
+              <li className='nav-bar__link'>Log out</li>
+              <li className='nav-bar__link'>Shop</li>
+            </>
+            :
+            <>
+              <li className='nav-bar__link'>Device</li>
+              <li className='nav-bar__link'>Shop</li>
+            </>
+        }
       </ul>
+      {
+        user.isAuth
+          ?
+          <li className='nav-bar__link'>Log in</li>
+          : 
+          <li className='nav-bar__link'>Log out</li>
+      }
     </div>
   )
 }
