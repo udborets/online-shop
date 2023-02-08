842fef2c8c2323b1806b6c5158d6178676581e60
Yury Borets <udborets@gmail.com>
2023-02-08 12:40:34 +0300

    add device slice

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
+import deviceReducer from "./deviceSlice";
 
 export default configureStore({
   reducer: {
     user: userReducer,
+    device: deviceReducer,
   }
 });
