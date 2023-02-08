be003561e591f8555db6c1fbb49511d4e41b1542
Yury Borets <udborets@gmail.com>
2023-02-08 14:40:20 +0300

    add footer in router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,13 +2,15 @@ import './App.scss'
 import { BrowserRouter } from 'react-router-dom';
 import AppRouter from './components/AppRouter/AppRouter';
 import NavBar from './components/NavBar/NavBar';
+import Footer from './components/Footer/Footer';
 
 
 function App() {
   return (
     <BrowserRouter>
       <NavBar />
-      <AppRouter />
+      <AppRouter/>
+      <Footer />
     </BrowserRouter>
   )
 }
