31f468affe1fcf6c6b641110a7434af78eeb37a8
Yury Borets <udborets@gmail.com>
2023-02-08 11:45:25 +0300

    add main page to routes

--- client/src/components/AppRouter.tsx
+++ client/src/components/AppRouter.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { IRoute } from '../models/IRoute';
 import { authRoutes, publicRoutes } from '../PageRoutes';
+import { RouteConsts } from '../utils/consts';
 
 const AppRouter = () => {
   const isAuth = false;
   return (
     <Routes>
+      <Route path="*" element={<Navigate to={RouteConsts.SHOP}/>}/>
       {isAuth && authRoutes.map(({ path, Component }: IRoute) => (
         <Route key={path} element={<Component />} path={path} />
       ))}
