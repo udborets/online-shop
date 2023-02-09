5dbe379a80e2a7df184824a86ec75669a86d5ccb
Yury Borets <udborets@gmail.com>
2023-02-09 17:48:52 +0300

    add type selecting

--- client/src/components/Type.tsx
+++ client/src/components/Type.tsx
@@ -1,9 +1,15 @@
+import { useSelector } from 'react-redux';
+import { IStore } from '../models/IStore';
 import { ITypeProps } from '../models/ITypeProps';
+import { useDevice } from './../hooks/useDevice';
 
-const TypeBarType = ({ children }: ITypeProps) => {
+const TypeBarType = ({ children, type }: ITypeProps) => {
+  const { selectType } = useDevice();
+  const devices = useSelector((state: IStore) => state.device);
   return (
     <div
-      className='type-bar__type'
+      className={type.id === devices.selectedType.id ? 'type-bar__type selected-type' : 'type-bar__type'}
+      onClick={() => selectType(type)}
     >
       {children}
     </div>
