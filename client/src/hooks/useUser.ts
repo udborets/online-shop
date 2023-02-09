8c37aa15a0a9bf126c4ca6d719f4528eb0ba68f1
Yury Borets <udborets@gmail.com>
2023-02-09 17:51:16 +0300

    correct useUser extension

--- client/src/hooks/useUser.ts
+++ client/src/hooks/useUser.ts
@@ -1,13 +1,12 @@
 import { useSelector, useDispatch } from "react-redux";
 import { setIsAuth } from "../store/userSlice";
-import { IStore } from './../models/IStore';
+import { IStore } from '../models/IStore';
 
 export function useUser() {
   const user = useSelector((state: IStore) => state.user);
   const dispatch = useDispatch();
   function toggleUserAuth() {
     dispatch(setIsAuth({ isAuth: !user.isAuth }));
-    console.log('User isAuth has changed to', user.isAuth);
   }
   return { user, toggleUserAuth }
 }
