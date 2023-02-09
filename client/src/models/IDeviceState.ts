1f9aedac2527762ad18c53fe4601d32a94cf2107
Yury Borets <udborets@gmail.com>
2023-02-09 17:51:40 +0300

    add selectedType field in device interface

--- client/src/models/IDeviceState.ts
+++ client/src/models/IDeviceState.ts
@@ -1,7 +1,10 @@
+import { IType } from "./IType";
+
 export interface IDeviceState {
   types: [{ id: number; name: string }];
   brands: [{ id: number; name: string }];
   devices: [
     { id: number; name: string; price: number; rating: number; img: string }
   ];
+  selectedType: IType;
 }
