bea80d482902efdf898a84262771d00048edda3f
Yury Borets <udborets@gmail.com>
2023-02-09 07:26:56 +0300

    add autocomplete to inputs

--- client/src/components/RegistrationInput.tsx
+++ client/src/components/RegistrationInput.tsx
@@ -4,7 +4,7 @@ import IRegistrationInput from './../models/IRegistrationInput';
 const RegistrationInput = ({ text, type }: IRegistrationInput) => {
   return (
     <div className='registration__input'>
-      <input type={type} placeholder={text} />
+      <input autoComplete="true" type={type} placeholder={text} />
     </div>
   )
 }
