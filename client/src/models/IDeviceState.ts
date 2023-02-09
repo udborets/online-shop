6d80d3a9a0b71c0ed521ea8b3cd37e29b3aa20b1
Yury Borets <udborets@gmail.com>
2023-02-09 17:06:44 +0300

    create device state interface

---
+++ client/src/models/IDeviceState.ts
@@ -0,0 +1,5 @@
+export interface IDeviceState {
+  types: [{ id: number; name: string }],
+  brands: [{id: number, name: string}],
+  devices: [{id: number, name: string, price: number, rating: number, img: string}],
+}
