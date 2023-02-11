866b42bbf5c38eee8cd78d07ab95633cb33bb96f
Yury Borets <udborets@gmail.com>
2023-02-11 12:51:33 +0300

    add device list component

--- client/src/components/DeviceList.tsx
+++ client/src/components/DeviceList.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { useSelector } from 'react-redux/es/exports';
+import { IStore } from './../models/IStore';
+import { IDevice } from './../models/IDevice';
+import Device from './Device';
+import '../styles/DeviceList.scss';
+
 
 const DeviceList = () => {
+  const devices = useSelector((state: IStore) => state.device.devices)
   return (
-    <div>DeviceList</div>
+    <div className='device-list'>
+      {devices.map((device: IDevice) => (
+        <Device key={device.id} device={device}></Device>
+      ))}
+    </div>
   )
 }
 
