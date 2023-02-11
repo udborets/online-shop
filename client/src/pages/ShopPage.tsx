1b8e8301188810a176542ea3d06fbc88d32f7a7d
Yury Borets <udborets@gmail.com>
2023-02-11 12:50:05 +0300

    add brandbar and device list to shoppage

--- client/src/pages/ShopPage.tsx
+++ client/src/pages/ShopPage.tsx
@@ -1,12 +1,19 @@
-import TypeBar from '../components/TypeBarComponent';
+import TypeBar from '../components/TypeBar';
 import '../styles/ShopPage.scss';
+import DeviceComponent from '../components/Device';
+import { useSelector } from 'react-redux/es/exports';
+import { IStore } from './../models/IStore';
+import DeviceList from '../components/DeviceList';
+import BrandBar from './../components/BrandBar';
 
 
 const ShopPage = () => {
+  const device = useSelector((state: IStore) => state.device)
   return (
     <div className='current-page shop-page'>
       <TypeBar />
-      
+      <BrandBar />
+      <DeviceList />
     </div>
   )
 }
