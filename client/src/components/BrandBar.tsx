c7945bcf486af377d32f3b3e14037123f621ba24
Yury Borets <udborets@gmail.com>
2023-02-11 12:50:52 +0300

    create brand bar component

---
+++ client/src/components/BrandBar.tsx
@@ -0,0 +1,25 @@
+import { useSelector } from 'react-redux/es/exports';
+import { IStore } from '../models/IStore';
+import Brand from './Brand';
+
+const BrandBar = () => {
+  const brands = useSelector((state: IStore) => state.device.brands)
+  return (
+    <div className='brand-bar'>
+      <div className="brand-bar__container">
+        {
+          brands.map((brand) => (
+            <Brand
+              key={brand.id}
+              brand={brand}
+            >
+              {brand.name}
+            </Brand>
+          ))
+        }
+      </div>
+    </div>
+  )
+}
+
+export default BrandBar
