720365f2d4c3a5f7e600c35e697b849695dc46d8
Yury Borets <udborets@gmail.com>
2023-02-11 13:27:16 +0300

    add auth-page class for auth page

--- client/src/pages/AuthPage.tsx
+++ client/src/pages/AuthPage.tsx
@@ -2,7 +2,7 @@ import RegistrationField from '../components/RegistrationField';
 
 const AuthPage = () => {
   return (
-    <div className='current-page'>
+    <div className='current-page auth-page'>
       <RegistrationField />
     </div>
   )
