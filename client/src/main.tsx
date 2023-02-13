57d52cc00b7e745212d41cba7ad6da0aff7cff5a
Yury Borets <udborets@gmail.com>
2023-02-13 19:15:50 +0300

    fix imports

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from "./store/index";
 import App from './App';
+import './styles/App.scss'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
