b5297507d0ba2dfdf2bd01e251ecb35ac80820a6
Yury Borets <udborets@gmail.com>
2023-02-13 19:16:40 +0300

    add device page

--- client/src/pages/DevicePage.tsx
+++ client/src/pages/DevicePage.tsx
@@ -1,6 +1,32 @@
+import "../styles/DevicePage.scss";
+import BuyButton from './../components/BuyButton';
+
 const DevicePage = () => {
+  const device = {
+    id: 6,
+    name: "Apple iphone 6",
+    price: 25000,
+    rating: 1,
+    img: "https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80",
+  };
+  
   return (
-    <div className='current-page'>DevicePage</div>
+    <div className='current-page device-page'>
+      <div className="device-page__main">
+        <div className="device-page__name">
+          {device.name}
+        </div>
+        <img
+          className="device-page__img"
+          src={device.img}
+          alt=""
+        />
+
+      </div>
+      <div className="device-page__column">
+        <BuyButton />
+      </div>
+    </div>
   )
 }
 
