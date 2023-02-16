cc4c498539735aa78cd79411bf59b43c25125e0c
Yury Borets <udborets@gmail.com>
2023-02-16 20:56:39 +0300

    create device rating stars component

---
+++ client/src/components/RatingStars.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import filledStar from  '../assets/filledStar.png';
+import star from  '../assets/star.png';
+import RatingStarsProps from '../models/IRatingStarsProps';
+
+const RatingStars = ({rating} : RatingStarsProps) => {
+  
+  return (
+    <div className='rating-stars'>
+      {[1, 2, 3, 4, 5].map((_, index) => {
+        if (index < rating) {
+          return <img key={index} className='rating-stars__star' src={filledStar} alt="" />;
+        }
+        return <img key={index} className='rating-stars__star' src={star} alt="" />;
+      })}
+    </div>
+  )
+}
+
+export default RatingStars
