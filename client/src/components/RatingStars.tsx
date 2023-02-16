6be6c431ec587d7f8808cc059403e1ad333d5226
Yury Borets <udborets@gmail.com>
2023-02-16 23:11:35 +0300

    fix rating stars import

--- client/src/components/RatingStars.tsx
+++ client/src/components/RatingStars.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 import filledStar from  '../assets/filledStar.png';
 import star from  '../assets/star.png';
 import RatingStarsProps from '../models/IRatingStarsProps';
+import '../styles/RatingStars.scss';
 
 const RatingStars = ({rating} : RatingStarsProps) => {
-  
   return (
     <div className='rating-stars'>
       {[1, 2, 3, 4, 5].map((_, index) => {
