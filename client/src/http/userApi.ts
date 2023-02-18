fcbda2963867176760497e8dd8d700d703d308f6
Yury Borets <udborets@gmail.com>
2023-02-18 20:47:16 +0300

    add try-catch for queries

--- client/src/http/userApi.ts
+++ client/src/http/userApi.ts
@@ -1,17 +1,47 @@
-import { $authHost, $host } from ".";
+import jwtDecode from "jwt-decode";
+import { $host } from ".";
+import { AxiosError } from 'axios';
 
 export const registration = async (email: string, password: string) => {
-  const response = await $host.post('api/user/registration', {email, password, role: "ADMIN"});
-  return response;
-}
+  try {
+    const { data } = await $host.post("api/user/registration", {
+      email,
+      password,
+      role: "ADMIN",
+    });
+    return jwtDecode(data.token);
+  } catch (err: unknown) {
+    if (err instanceof AxiosError) {
+      return err;
+    }
+    else {
+      console.error(err);
+    }
+  }
+};
 export const login = async (email: string, password: string) => {
-  const response = await $host.post('api/user/login', {email, password});
-  return response;
-}
+  try {
+    const { data } = await $host.post("api/user/login", { email, password });
+    return jwtDecode(data.token);
+  } catch (err: unknown) {
+    if (err instanceof AxiosError) {
+      return err;
+    }
+    else {
+      console.error(err);
+    }
+  }
+};
 export const check = async () => {
-  const response = await $host.post('api/auth/registration');
-  return response;
-}
-
-
-
+  try {
+    const response = await $host.post("api/auth/registration");
+    return response;
+  } catch (err: unknown) {
+    if (err instanceof AxiosError) {
+      return err;
+    }
+    else {
+      console.error(err);
+    }
+  }
+};
