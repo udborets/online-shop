1d9db49f42a383e7d7298e7420a1bb2681eb7131
Yury Borets <udborets@gmail.com>
2023-02-18 12:44:21 +0300

    create modal window interface

---
+++ client/src/models/IModal.ts
@@ -0,0 +1,7 @@
+import { ReactElement } from "react";
+
+export default interface IModal {
+  active: boolean;
+  setActive: React.Dispatch<React.SetStateAction<boolean>>;
+  children: ReactElement<any, any>;
+}
