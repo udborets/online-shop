7e1da19b919e7bb533dbfd69016ca5613312656d
Yury Borets <udborets@gmail.com>
2023-02-18 20:45:30 +0300

    add logout modal window to app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,11 +2,19 @@ import { BrowserRouter } from 'react-router-dom';
 import AppRouter from './components/AppRouter';
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
+import { useUser } from './hooks/useUser';
+import LogoutSure from './components/modals/LogOutSure';
+
 
 
 const App = () => {
+  const user = useUser();
   return (
     <BrowserRouter>
+      <LogoutSure
+        active={user.user.isShowingLogout}
+        setActive={user.toggleWannaLogout}
+      />
       <NavBar />
       <AppRouter />
       <Footer />
