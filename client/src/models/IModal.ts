974dd6372517c619f3819d491b05703a423c335e
Yury Borets <udborets@gmail.com>
2023-02-18 20:33:50 +0300

    fix modal props interface

--- client/src/models/IModal.ts
+++ client/src/models/IModal.ts
@@ -3,5 +3,4 @@ import { ReactElement } from "react";
 export default interface IModal {
   active: boolean;
   setActive: React.Dispatch<React.SetStateAction<boolean>>;
-  children: ReactElement<any, any>;
 }
