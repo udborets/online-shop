967f593770b59f31f0119faf90f2f1559a942bd3
Yury Borets <udborets@gmail.com>
2023-02-18 12:46:14 +0300

    add basic components to admin

--- client/src/pages/AdminPage.tsx
+++ client/src/pages/AdminPage.tsx
@@ -1,11 +1,35 @@
 import '../styles/AdminPage.scss';
+import CreateDevice from './../components/modals/CreateDevice';
+import CreateType from './../components/modals/CreateType';
+import CreateBrand from './../components/modals/CreateBrand';
+import { useState } from 'react';
+import AdminButton from './../components/ui/AdminButton';
 
 const AdminPage = () => {
+  const [typeActive, setTypeActive] = useState(false);
+  const [brandActive, setBrandActive] = useState(false);
+  const [deviceActive, setDeviceActive] = useState(false);
+
   return (
     <div className='current-page'>
-      <button className='admin-btn'>Add type</button>
-      <button className='admin-btn'>Add brand</button>
-      <button className='admin-btn'>Add device</button>
+      <AdminButton onClick={() => setDeviceActive(old => !old)} text={'Create device'} />      
+      <AdminButton onClick={() => setTypeActive(old => !old)} text={'Create type'} />      
+      <AdminButton onClick={() => setBrandActive(old => !old)} text={'Create brand'} />      
+      <CreateDevice active={deviceActive} setActive={setDeviceActive} >
+        <div>
+          device
+        </div>
+      </CreateDevice>
+      <CreateType active={typeActive} setActive={setTypeActive} >
+        <div>
+          type
+        </div>
+      </CreateType>
+      <CreateBrand active={brandActive} setActive={setBrandActive} >
+        <div>
+          brand
+        </div>
+      </CreateBrand>
     </div>
   )
 }
