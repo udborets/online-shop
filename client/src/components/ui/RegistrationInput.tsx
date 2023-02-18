694ca597ff5f41e93f69bd4d4f4e1b5fe9ef8ebf
Yury Borets <udborets@gmail.com>
2023-02-18 16:00:07 +0300

    add functionality to registration inputs

--- client/src/components/ui/RegistrationInput.tsx
+++ client/src/components/ui/RegistrationInput.tsx
@@ -1,10 +1,16 @@
 import IRegistrationInput from '../../models/IRegistrationInput';
 
 
-const RegistrationInput = ({ text, type }: IRegistrationInput) => {
+const RegistrationInput = ({ text, type, value, setValue }: IRegistrationInput) => {
   return (
     <div className='registration__input'>
-      <input autoComplete="true" type={type} placeholder={text} />
+      <input
+        autoComplete="true"
+        type={type}
+        placeholder={text}
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        />
     </div>
   )
 }
