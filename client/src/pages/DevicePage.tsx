a24aa3d3e394ae829bfe61c1f78aa766c7d9c2f3
Yury Borets <udborets@gmail.com>
2023-02-19 20:37:08 +0300

    add getting device info from server to deivce page

--- client/src/pages/DevicePage.tsx
+++ client/src/pages/DevicePage.tsx
@@ -2,41 +2,56 @@ import BuyButton from '../components/ui/BuyButton';
 import "../styles/DevicePage.scss";
 import '../components/RatingStars';
 import RatingStars from './../components/RatingStars';
-import { useDevice } from './../hooks/useDevice';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { fetchDevice } from './../http/deviceApi';
+import { useParams } from 'react-router-dom';
+import { IDevice } from '../models/IDevice';
+import { useState, useEffect } from 'react';
+import { RouteConsts } from '../utils/consts';
 
 const DevicePage = () => {
-  const description = [
-    {id: 1, title: 'helloing 1', description: 'baba'},
-    {id: 2, title: 'helloing 2', description: 'baba'},
-    {id: 3, title: 'helloing 3', description: 'baba'},
-    {id: 4, title: 'helloing 4', description: 'baba'},
-  ]
-  const path = useLocation().pathname.split('/'); //
-  const deviceId = Number.parseInt(path[path.length - 1]); //
-  const device = useDevice().getDeviceById(deviceId); //
+  const { id } = useParams();
+  const navigate = useNavigate();
+  if (id === undefined) {
+    navigate(RouteConsts.SHOP);
+  }
+  const [device, setDevice] = useState<IDevice>({
+    id: 0,
+    name: '',
+    price: 0,
+    rating: 0,
+    img: '',
+    info: [{ id: 0, title: '', description: '' }]
+  });
+
+  useEffect(() => {
+    fetchDevice(id ?? "")
+      .then((fetchedDevice) => {
+        setDevice(fetchedDevice);
+      })
+  })
   return (
     <div className='current-page device-page'>
       <div className="device-page__main">
         <div className="device-page__title">
-          {device.name}
+          {device && device.name}
         </div>
         <img
           loading='lazy'
           className="device-page__img"
-          src={device.img}
+          src={device && import.meta.env.VITE_REACT_APP_API_URL + device.img}
           alt=""
         />
       </div>
       <div className="device-page__column">
-          <RatingStars rating={device.rating}/>
+        <RatingStars rating={device?.rating ?? 0} />
         <BuyButton />
       </div>
       <ul className='device-page__description'>
-        {description.map((el) => (
-        <li key={el.id}>
-          {el.title} : {el.description}
-        </li>))}
+        {device.info.map((el) => (
+          <li key={el.id}>
+            {el.title} : {el.description}
+          </li>))}
       </ul>
     </div>
   )
