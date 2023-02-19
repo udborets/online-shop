085f63fd9fa8650cb6cd10b2c549d2f93aad513d
Yury Borets <udborets@gmail.com>
2023-02-19 20:15:55 +0300

    fix shop page device displaying

--- client/src/components/modals/CreateDevice.tsx
+++ client/src/components/modals/CreateDevice.tsx
@@ -12,12 +12,13 @@ const CreateDevice = ({ active, setActive }: IModal) => {
   const deleteInfo = (id: number) => {
     setInfo(info.filter((i: any) => i.id !== id))
   }
+
   return (
     <div className={active ? 'modal active' : 'modal'} onClick={() => setActive(false)}>
       <div className='modal__container' onClick={e => e.stopPropagation()}>
         <select>
           <option value="">--select a brand--</option>
-          {device.device.brands.map(brand => {
+          {device.device.brands.length && device.device.brands.map(brand => {
             if (brand.id !== -1)
               return (
                 <option value={brand.name} key={"brand " + brand.id}>{brand.name}</option>
@@ -26,7 +27,7 @@ const CreateDevice = ({ active, setActive }: IModal) => {
         </select>
         <select>
           <option>--select a type--</option>
-          {device.device.types.map(type => {
+          {device.device.types.length && device.device.types.map(type => {
             if (type.id !== -1)
               return (
                 <option value={type.name} key={"brand " + type.id}>{type.name}</option>
