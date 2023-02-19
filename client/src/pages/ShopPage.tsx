373e6e7787006a1b9cd14ae4194ea0e71dc90a7d
Yury Borets <udborets@gmail.com>
2023-02-19 18:35:00 +0300

    delete unused imports

--- client/src/pages/ShopPage.tsx
+++ client/src/pages/ShopPage.tsx
@@ -2,20 +2,8 @@ import TypeBar from '../components/TypeBar';
 import DeviceList from '../components/DeviceList';
 import BrandBar from './../components/BrandBar';
 import '../styles/ShopPage.scss';
-import { useEffect } from 'react';
-import { useDevice } from './../hooks/useDevice';
-import { useSelector } from 'react-redux';
-import { IStore } from './../models/IStore';
 
 const ShopPage = () => {
-  const brand = useDevice();
-  const device = useSelector((state: IStore) => state.device);
-
-  useEffect(() => {
-    brand.selectBrand(device.brands[0]);
-    brand.selectType(device.types[0])
-  }, []);
-
   return (
     <div className='current-page shop-page'>
       <TypeBar />
