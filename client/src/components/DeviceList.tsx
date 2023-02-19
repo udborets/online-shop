387da50744c570e1207a8f210777003b58435af6
Yury Borets <udborets@gmail.com>
2023-02-19 20:05:42 +0300

    add updating devices state

--- client/src/components/DeviceList.tsx
+++ client/src/components/DeviceList.tsx
@@ -1,15 +1,19 @@
-import { useSelector } from 'react-redux/es/exports';
-import { IStore } from './../models/IStore';
 import { IDevice } from './../models/IDevice';
 import Device from './DeviceItem';
 import '../styles/DeviceList.scss';
+import { useDevice } from '../hooks/useDevice';
+import { useEffect } from 'react';
 
 
 const DeviceList = () => {
-  const devices = useSelector((state: IStore) => state.device.devices);
+  const { device, updateDevices } = useDevice();
+  useEffect(() => {
+    updateDevices();
+  }, [])
+  if(!device.devices.length) return <div>There are no devices yet</div>
   return (
     <div className='device-list'>
-      {devices.map((device: IDevice) => (
+      {device.devices.map((device: IDevice) => (
         <Device key={device.id} device={device}></Device>
       ))}
     </div>
