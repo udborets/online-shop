04efcf9878b01a96d2be6632958d4a3960a98d7f
Yury Borets <udborets@gmail.com>
2023-02-19 20:06:59 +0300

    create adding brand function

--- client/src/components/modals/CreateBrand.tsx
+++ client/src/components/modals/CreateBrand.tsx
@@ -1,11 +1,30 @@
 import IModal from './../../models/IModal';
 import '../../styles/Modal.scss';
+import { useState } from 'react';
+import { createBrand } from './../../http/deviceApi';
 
 const CreateBrand = ({ active, setActive }: IModal) => {
+  const [value, setValue] = useState('');
+  const addBrand = () => {
+    createBrand(value)
+      .then(() => {
+        setValue('');
+      })
+  }
   return (
     <div className={active ? 'modal active' : 'modal'} onClick={() => setActive(false)}>
       <div className='modal__container' onClick={e => e.stopPropagation()}>
+        <input
+          type="text"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
 
+        />
+        <button
+          onClick={() => addBrand()}
+        >
+          Add brand
+        </button>
       </div>
     </div>
   )
