141745ceb150ba7fe92cbc75a60213aadc4deeab
Yury Borets <udborets@gmail.com>
2023-02-19 20:07:05 +0300

    create adding type function

--- client/src/components/modals/CreateType.tsx
+++ client/src/components/modals/CreateType.tsx
@@ -1,11 +1,30 @@
 import IModal from './../../models/IModal';
 import '../../styles/Modal.scss';
+import { useState } from 'react';
+import { createType } from './../../http/deviceApi';
 
 const CreateType = ({ active, setActive }: IModal) => {
+  const [value, setValue] = useState('');
+
+  const addType = () => {
+    createType(value)
+      .then(() => {
+        setValue('');
+      })
+  }
   return (
     <div className={active ? 'modal active' : 'modal'} onClick={() => setActive(false)}>
       <div className='modal__container' onClick={e => e.stopPropagation()}>
-
+        <input
+          type="text"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <button
+          onClick={() => addType()}
+        >
+          Add type
+        </button>
       </div>
     </div>
   )
