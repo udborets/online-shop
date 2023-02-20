d7061d4d64cf479dc5fd94b976beab296da6a50c
Yury Borets <udborets@gmail.com>
2023-02-20 20:38:30 +0300

    add brand global state update

--- client/src/components/modals/CreateBrand.tsx
+++ client/src/components/modals/CreateBrand.tsx
@@ -2,14 +2,17 @@ import IModal from './../../models/IModal';
 import '../../styles/Modal.scss';
 import { useState } from 'react';
 import { createBrand } from './../../http/deviceApi';
+import { useDevice } from '../../hooks/useDevice';
 
 const CreateBrand = ({ active, setActive }: IModal) => {
   const [value, setValue] = useState('');
+  const { updateBrands } = useDevice();
   const addBrand = () => {
     createBrand(value)
       .then(() => {
         setValue('');
       })
+    updateBrands();
   }
   return (
     <div className={active ? 'modal active' : 'modal'} onClick={() => setActive(false)}>
@@ -18,7 +21,6 @@ const CreateBrand = ({ active, setActive }: IModal) => {
           type="text"
           value={value}
           onChange={(e) => setValue(e.target.value)}
-
         />
         <button
           onClick={() => addBrand()}
