b0243135a150c2172dbfc585a12f89b5487722d1
Yury Borets <udborets@gmail.com>
2023-02-20 20:41:38 +0300

    add type state update on type update

--- client/src/components/modals/CreateType.tsx
+++ client/src/components/modals/CreateType.tsx
@@ -2,15 +2,17 @@ import IModal from './../../models/IModal';
 import '../../styles/Modal.scss';
 import { useState } from 'react';
 import { createType } from './../../http/deviceApi';
+import { useDevice } from '../../hooks/useDevice';
 
 const CreateType = ({ active, setActive }: IModal) => {
   const [value, setValue] = useState('');
-
+  const { updateTypes } = useDevice();
   const addType = () => {
     createType(value)
       .then(() => {
         setValue('');
       })
+    updateTypes();
   }
   return (
     <div className={active ? 'modal active' : 'modal'} onClick={() => setActive(false)}>
