25a1ccd034ff647e1bb5f1d495e0dcc68d9da952
Yury Borets <udborets@gmail.com>
2023-02-20 22:37:00 +0300

    add custom hook usage

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -3,17 +3,17 @@ import { useUser } from './../hooks/useUser';
 
 
 const Footer = () => {
-  const { user, toggleUserAuth } = useUser()
+  const { user, toggleUserAuth, isAuth } = useUser()
 
   return (
     <div className='footer'>
       <div className="footer__container">
         <button
-          onClick={() => toggleUserAuth(!user.isAuth)}
+          onClick={() => toggleUserAuth(!isAuth)}
         >
-          dev: Now: {user.isAuth ? "auth" : "not auth"}
+          dev: Now: {isAuth ? "auth" : "not auth"}
         </button>
-        <button onClick={() => console.log(user.user)}>dev: Log user info</button>
+        <button onClick={() => console.log(user)}>dev: Log user info</button>
         <a className='footer__link' href="https://t.me/udborets">My Telegram</a>
         <a className='footer__link' href="https://github.com/udborets/">My Github</a>
       </div>
