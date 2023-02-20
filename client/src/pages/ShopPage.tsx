0e91341b8c943a28011cfd01679d8c408d37a7ae
Yury Borets <udborets@gmail.com>
2023-02-20 20:46:17 +0300

    add states update on shop page load

--- client/src/pages/ShopPage.tsx
+++ client/src/pages/ShopPage.tsx
@@ -2,8 +2,16 @@ import TypeBar from '../components/TypeBar';
 import DeviceList from '../components/DeviceList';
 import BrandBar from './../components/BrandBar';
 import '../styles/ShopPage.scss';
+import { useEffect } from 'react';
+import { useDevice } from '../hooks/useDevice';
 
 const ShopPage = () => {
+  const {updateBrands, updateDevices, updateTypes} = useDevice();
+  useEffect(() => {
+    updateDevices();
+    updateBrands();
+    updateTypes();
+  }, [])
   return (
     <div className='current-page shop-page'>
       <TypeBar />
